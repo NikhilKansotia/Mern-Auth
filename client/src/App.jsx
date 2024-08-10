909b3ed9db0af45de938f22f0649c0d1334b2dc8
NikhilKansotia <kansotianikhil92@gmail.com>
2024-08-10 11:28:56 +0530

    Add routes and create changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signup from "./Pages/Signup";
+import Signin from "./Pages/Signin";
+import Profile from "./Pages/Profile";
+import About from "./Pages/About";
+
 function App() {
-  return <div className="bg-red-400">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
